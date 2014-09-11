6188355133a5bba53ccba10262e519f222b66740
Carlos L Gonzalez <carloslgonzalez.pr@gmail.com>
2014-09-11 16:53:58 -0400

    Added from and reply to

--- assets/subscribe.php
+++ assets/subscribe.php
@@ -30,8 +30,8 @@ if($_POST) {
 	    $subject = 'New Subscriber (seria)!';
 	    $body = "You have a new subscriber!\n\nEmail: " . $subscriber_email;
         // uncomment this to set the From and Reply-To emails, then pass the $headers variable to the "mail" function below
-	    //$headers = "From: ".$subscriber_email." <" . $subscriber_email . ">" . "\r\n" . "Reply-To: " . $subscriber_email;
-	    mail($emailTo, $subject, $body);
+	    $headers = "From: ".$subscriber_email." <" . $subscriber_email . ">" . "\r\n" . "Reply-To: " . $subscriber_email;
+	    mail($emailTo, $headers, $subject, $body);
     }
 
 }
